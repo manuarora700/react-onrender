7b22febaf578c3684654c0e946554bbb80b855e1
Manu Arora <manuarorawork@gmail.com>
2022-02-24 11:52:45 +0530

    Test manu v4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Develop branch
+          Edit <code>src/App.js</code> and save to reload. Develop branch. Check my deploy PRs.
         </p>
         <a
           className="App-link"
